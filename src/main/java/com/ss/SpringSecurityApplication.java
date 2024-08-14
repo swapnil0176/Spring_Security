3606a3888161ef51e292a3fb3942e44b57cc5422
Swapnil_Tawhare <136835903+swapnil0176@users.noreply.github.com>
2024-08-14 22:24:05 +0530

    Add files via upload

---
+++ src/main/java/com/ss/SpringSecurityApplication.java
@@ -0,0 +1,15 @@
+package com.ss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityApplication {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(SpringSecurityApplication.class, args);
+		System.out.println("Hello from Spring Security");
+	}
+
+}
